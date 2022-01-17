17e85339bc2b58d5a301352baadec32c7332f8a7
Qian Li <qian.li10000@gmail.com>
2022-01-17 00:20:00 -0600

    Update lh2_match.R

--- R/lh2_match.R
+++ R/lh2_match.R
@@ -1,7 +1,7 @@
 #' @export
 
 lh2_match<-function(o,alpha,z,rand_sub,rand_set){
-  quad.n<-3
+  quad.n<-5
   alpha=matrix(rep(alpha,quad.n^3),ncol = quad.n^3)
   prob=1/(1+exp(-(z%*%alpha+rand_sub+rand_set)))
   l2=o*prob+(1-o)*(1-prob)
