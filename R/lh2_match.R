cad9490bd4f15074d616ef71531e2c385be402b1
Qian Li <qian.li10000@gmail.com>
2022-03-13 14:34:21 -0500

    Delete lh2_match.R

--- R/lh2_match.R
+++
@@ -1,9 +0,0 @@
-#' @export
-
-lh2_match<-function(o,alpha,z,rand_sub,rand_set){
-  quad.n<-4
-  alpha=matrix(rep(alpha,quad.n^3),ncol = quad.n^3)
-  prob=1/(1+exp(-(z%*%alpha+rand_sub+rand_set)))
-  l2=o*prob+(1-o)*(1-prob)
-  return(l2)
-}
